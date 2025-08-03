e2fc4cdb16975ea999d6577a7acbd7b20c9c5de6
Sacha Muszlak <sacha.muszlak@inrae.fr>
2025-08-03 16:20:18 +0200

    Add all changes

---
+++ Configs/doom/.config/doom/custom.el
@@ -0,0 +1,17 @@
+;;; -*- lexical-binding: t -*-
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("5c8a1b64431e03387348270f50470f64e28dfae0084d33108c33a81c1e126ad6"
+     "5244ba0273a952a536e07abaad1fdf7c90d7ebb3647f36269c23bfd1cf20b0b8" default))
+ '(package-selected-packages
+   '(org-fragtog org-plus-contrib org-ref org-ref-prettify org-super-agenda)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
